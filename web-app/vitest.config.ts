a8aa07663dfabaec90065b9f5e6590e31555f63e
m4gr4th34 <irfan.alikhan@gmail.com>
2026-02-19 17:33:22 -0800

    fixing CI for e2e tests

--- web-app/vitest.config.ts
+++ web-app/vitest.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig, configDefaults } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
@@ -7,5 +7,7 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['./src/test/setup.ts'],
     globals: true,
+    include: ['**/*.test.{ts,tsx}'],
+    exclude: [...configDefaults.exclude, '**/tests/**'],
   },
 })
